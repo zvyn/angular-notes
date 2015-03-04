2c89b8796b4b58debb04c3c807cadb8cd5b6305f
Milan Oberkirch <zvyn@oberkirch.org>
2015-03-05 00:26:20 +1300

    title and slogan are stored too

--- js/app.js
+++ js/app.js
@@ -2,30 +2,43 @@
     var app = angular.module('note', [ ]);
 
     app.controller('NotesController', ['$http', function($http){
-        this.notes = JSON.parse(localStorage["notes"]);
-        this.notes = this.notes ? this.notes : [];
-        this.maxID = parseInt(localStorage["maxID"]);
-        this.maxID = this.maxID ? this.maxID : 0;
-        this.saveToLocalData = function() {
-            localStorage["maxID"] = JSON.stringify(this.maxID);
-            localStorage["notes"] = JSON.stringify(this.notes);
+        this.parse = function (attr, default_value) {
+            value = localStorage[attr];
+            if (value != undefined) {
+                this[attr] = JSON.parse(value);
+            }
+            else {
+                this[attr] = default_value;
+            }
         };
-        this.submit = function(note){
-            this.saveToLocalData();
-           };
+
+        this.save = function(attr) {
+            localStorage[attr] = JSON.stringify(this[attr]);
+        }
+
+        this.submit = function() {
+            this.save("notes");
+            this.save("maxID");
+        };
+
         this.deleteNote = function(note){
             var index = this.notes.findIndex(function(candidate){
                 return candidate["title"] == note["title"];
             });
             this.notes.splice(index, 1);
-            this.saveToLocalData();
+            this.save("notes");
         };
+
         this.addNote = function(){
             var note = {'id': this.maxID + 1, 'title': '', 'body': ''};
             this.maxID += 1;
             this.notes.push(note);
         };
-        notesController = this;
+
+        this.parse("notes", []);
+        this.parse("maxID", 0);
+        this.parse("title", "Notes");
+        this.parse("slogan", "anything can change");
     }]);
 
     app.directive('overview', function(){
