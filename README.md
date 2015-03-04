1c5620a3f997e79d1ce1757b2277e3cbe4508e9a
Milan Oberkirch <zvyn@oberkirch.org>
2015-03-04 23:24:29 +1300

    saving objects to localStorage now

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Notes
 =====
 
 Notes is a small notebook webapp written in AngularJs. There is no backand
-(yet) so instead of saving objects they are dumped to 'console.log'.
+(yet) so instead of saving objects they are written to `localStorage`.
 
 This is basically my AngularJs playground to become fmiliar with the framework.
 
--- js/app.js
+++ js/app.js
@@ -2,29 +2,30 @@
     var app = angular.module('note', [ ]);
 
     app.controller('NotesController', ['$http', function($http){
-        this.notes = [];
-        this.maxID = 0;
+        this.notes = JSON.parse(localStorage["notes"]);
+        this.notes = this.notes ? this.notes : [];
+        this.maxID = parseInt(localStorage["maxID"]);
+        this.maxID = this.maxID ? this.maxID : 0;
+        this.saveToLocalData = function() {
+            localStorage["maxID"] = JSON.stringify(this.maxID);
+            localStorage["notes"] = JSON.stringify(this.notes);
+        };
         this.submit = function(note){
-            console.log("save", note, "(not implemented)");
-            };
+            this.saveToLocalData();
+           };
         this.deleteNote = function(note){
             var index = this.notes.findIndex(function(candidate){
                 return candidate["title"] == note["title"];
             });
             this.notes.splice(index, 1);
-            console.log("delete", note, "(not implemented)");
-        }
+            this.saveToLocalData();
+        };
         this.addNote = function(){
             var note = {'id': this.maxID + 1, 'title': '', 'body': ''};
             this.maxID += 1;
             this.notes.push(note);
-            console.log("add", note, "(not implemented)");
         };
         notesController = this;
-        $http.get('api/notes.json').success(function(data){
-            notesController.notes = data["notes"];
-            notesController.maxID = data["maxID"];
-        });
     }]);
 
     app.directive('overview', function(){
